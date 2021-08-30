9a4ded790a3f5b5d2b933c29270c38f64bb6fa9a
Aloola <alo.virunurm@gmail.com>
2021-08-30 13:47:33 +0300

    multiple players

---
+++ sandclient.py
@@ -0,0 +1,98 @@
+import socket               # Import socket module
+from time import sleep
+import pygame
+import threading
+from materials import *
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+
+
+
+jah = True
+cells = []
+
+s = socket.socket()
+try:
+    s.connect((host, port))
+except Exception as e:
+    print("oi ei, ", e)
+    jah = False
+
+
+def draw_materials(surface, materials, material_size, selected, offset):
+    pygame.draw.rect(surface, col_selected, (offset, selected * material_size, material_size, material_size))
+    for key in material_dict:
+        pygame.draw.rect(surface, material_dict[key].color, (offset + 5, int(key) * material_size + 5, material_size - 10, material_size - 10))
+
+def get_cells():
+
+    global cells
+    
+    while True:
+        try:
+            message = s.recv(1048576)
+            cells = eval(message)
+        except:
+            print("oi ei ", message, " on arusaamatu")
+
+def send_click(mouse_pos, buttons, material):
+    s.sendall(bytes(str([mouse_pos, buttons, material]), encoding = "utf-8"))
+
+def main():
+    pygame.init()
+    surface = pygame.display.set_mode((squares_x * square_size + material_size, max(squares_y * square_size, material_size * num_materials)))
+    selected = 0
+    line_start = (-1, -1)
+
+    global cells
+    
+    while True:
+        pygame.time.Clock().tick(UPS)
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        pressed = pygame.mouse.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed() == (1, 0, 0):
+                    if squares_x * square_size < mouse_x < squares_x * square_size + material_size and 0 < mouse_y < material_size * num_materials:
+                        selected = mouse_y//material_size
+                
+                elif pygame.mouse.get_pressed() == (0, 1, 0):
+                    if line_start == (-1, -1):
+                        line_start = pygame.mouse.get_pos()
+                        line_start = (line_start[0] // square_size, line_start[1] // square_size)
+                    else:
+                        line_end = pygame.mouse.get_pos()
+                        line_end = (line_end[0] // square_size, line_end[1] // square_size)
+                        send_click((line_start[0], line_start[1], line_end[0], line_end[1]), pygame.mouse.get_pressed(), selected)
+                            
+                        line_start = (-1, -1)
+
+        if pressed != (False, False, False) and 0 < mouse_x < squares_x * square_size and 0 < mouse_y < squares_y * square_size:
+            send_click((mouse_x // square_size, mouse_y // square_size), pressed, selected)
+
+        surface.fill(col_background)
+        pygame.draw.rect(surface, col_empty, (0, 0, squares_x * square_size, squares_y * square_size))
+
+        draw_materials(surface, material_dict, material_size, selected, squares_x * square_size)
+            
+        for x, y, color in cells:
+            pygame.draw.rect(surface, color, (x*square_size, y*square_size, square_size-1, square_size-1))
+        
+        if line_start != (-1, -1):
+            pygame.draw.circle(surface, col_selected, ((line_start[0]+0.5) * square_size, (line_start[1]+0.5) * square_size), 10)
+            pygame.draw.line(surface, col_selected, ((line_start[0]+0.5) * square_size, (line_start[1]+0.5) * square_size), ((pygame.mouse.get_pos()[0] // square_size + 0.5) * square_size, (pygame.mouse.get_pos()[1] // square_size + 0.5) * square_size))
+
+        pygame.display.update()
+
+if jah:
+    thread1 = threading.Thread(target = get_cells)
+    thread2 = threading.Thread(target = main)
+
+    thread1.start()
+    thread2.start()
---
+++ sandserver.py
@@ -0,0 +1,152 @@
+import pygame
+from materials import *
+import numpy as np
+from random import randint
+import socket
+from time import sleep
+import threading
+
+PAUSED = 0
+cells = []
+
+def init(dimx, dimy):
+    cells_grid = np.zeros((dimy, dimx), dtype="O")
+    return (cells_grid)
+
+cells_grid = init(squares_x, squares_y)
+
+def draw_materials(surface, materials, material_size, selected, offset):
+    pygame.draw.rect(surface, col_selected, (offset, selected * material_size, material_size, material_size))
+    for key in material_dict:
+        pygame.draw.rect(surface, material_dict[key].color, (offset + 5, int(key) * material_size + 5, material_size - 10, material_size - 10))
+
+def respond():
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 12345
+    s.bind((host, port))
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        thread = threading.Thread(target = new_client, args = (c,addr))
+        thread.start()
+
+def new_client(c,addr):
+    global cells
+    thread = threading.Thread(target = get_clicks, args = (c,addr))
+    thread.start()
+    while True:
+        cells_send = str([[cell.x, cell.y, cell.color] for cell in cells]) + "#"
+        c.sendall(bytes(cells_send, encoding = "utf-8"))
+        sleep(1/UPS)
+
+def get_clicks(c, addr):
+    global cells, cells_grid, squares_x, squares_y
+    while True:
+        click = eval(c.recv(1024))
+        if len(click[0]) == 4:
+            print(click)
+            for pos in get_line((click[0][0], click[0][1]), (click[0][2], click[0][3])):
+                if get_cell(pos[0], pos[1], cells_grid, squares_y, squares_x).state == "":
+                    set_cell(pos[0], pos[1], cells, cells_grid, material_dict[str(click[2])](pos[0], pos[1]))
+            
+        if click[1] == (0, 0, 1):
+            cell = get_cell(click[0][0], click[0][1], cells_grid, squares_y, squares_x)
+            if cell.state not in ("", "1"):
+                remove_cell(cell, cells, cells_grid)
+        elif click[1] == (1, 0, 0):
+            if get_cell(click[0][0], click[0][1], cells_grid, squares_y, squares_x).state == "":
+                set_cell(click[0][0], click[0][1], cells, cells_grid, material_dict[str(click[2])](click[0][0], click[0][1]))
+        
+
+def get_line(start, end): # not mine
+    x1, y1 = start
+    x2, y2 = end
+    dx = x2 - x1
+    dy = y2 - y1
+ 
+    # Determine how steep the line is
+    is_steep = abs(dy) > abs(dx)
+ 
+    # Rotate line
+    if is_steep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+ 
+    # Swap start and end points if necessary and store swap state
+    swapped = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        swapped = True
+ 
+    # Recalculate differentials
+    dx = x2 - x1
+    dy = y2 - y1
+ 
+    # Calculate error
+    error = int(dx / 2.0)
+    ystep = 1 if y1 < y2 else -1
+ 
+    # Iterate over bounding box generating points between start and end
+    y = y1
+    points = []
+    for x in range(x1, x2 + 1):
+        coord = (y, x) if is_steep else (x, y)
+        points.append(coord)
+        error -= abs(dy)
+        if error < 0:
+            y += ystep
+            error += dx
+ 
+    # Reverse the list if the coordinates were swapped
+    if swapped:
+        points.reverse()
+    return points
+
+def main(dimx, dimy, cellsize):
+    pygame.init()
+    clock = pygame.time.Clock()
+
+
+    line_start = (-1, -1)
+
+    selected = 0
+
+    global PAUSED
+    global cells
+
+    while True:
+        clock.tick(UPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    PAUSED = not PAUSED
+                if event.key == pygame.K_RETURN:
+                    PAUSED = -1
+
+        if PAUSED < 1:
+            for cell in cells:
+                cell.update(cells, cells_grid, dimx, dimy)
+
+        if PAUSED == -1:
+            PAUSED = 1
+
+        for cell in cells:
+            if get_cell(cell.x, cell.y, cells_grid, dimx, dimy) != cell:
+                print(cell.x, cell.y, "is wrong!")
+                print(cells_grid)
+                throwexception()
+
+def start():
+    main(squares_x, squares_y, square_size)
+
+thread1 = threading.Thread(target = respond)
+thread2 = threading.Thread(target = start)
+
+thread1.start()
+thread2.start()
